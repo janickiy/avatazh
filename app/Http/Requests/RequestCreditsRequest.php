fe103523972ab6d5acbe9f4ef07b8a53cbc9afd8
janickiy <janickiy@mail.ru>
2017-12-27 17:02:32 +0300

    bug fix

--- app/Http/Requests/RequestCreditsRequest.php
+++ app/Http/Requests/RequestCreditsRequest.php
@@ -6,15 +6,6 @@ use App\Http\Requests\Request;
 
 class RequestCreditsRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
 
     /**
      * Get the validation rules that apply to the request.
